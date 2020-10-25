646d734bf6245df1ffb30a5d9f558f52169408c3
Olivier Lutz <olivierlutz8@gmail.com>
2020-10-25 19:16:45 +0000

    ajout DELETE revenu et message d'ajout revenu personnalisé

--- app/controllers/revenus_controller.rb
+++ app/controllers/revenus_controller.rb
@@ -1,5 +1,34 @@
 class RevenusController < ApplicationController
+  include ActionView::Helpers::NumberHelper
   def index
      @caca = Revenu.all
+     @cacaNew = Revenu.new
   end
+  
+  def create
+    @cacaNew = Revenu.new(revenu_params)
+    @cacaNew.save
+    salaire = number_to_currency(Revenu.last.salaire, :unit => "€", locale: :fr, precision: 0)
+    revenuindep = number_to_currency(Revenu.last.revenuindep, :unit => "€", locale: :fr, precision: 0)
+      redirect_to revenus_path, notice: "Revenu indépendant de " + revenuindep + " et salarié de " + salaire + " a bien été enregistré en base de donnée"
+  end
+  
+  def destroy
+    @revenu = Revenu.find(params[:id])
+    p = params[:revenu].inspect
+    @revenu.destroy
+  end
+    
+  def show
+    @revenu = Revenu.find(params[:id])
+    p = params[:revenu].inspect
+    redirect_to revenu_path, notice: (p + " ==> a été supprimé de la base de donnée")
+  end
+  
+  private
+  
+  def revenu_params
+    params.require(:revenu).permit(:salaire, :revenuindep)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'revenus/index'
   root to: 'fraispros#index'
   get 'duplicate/:id', to: 'fraispros#duplicate', as: 'duplicate'
+  resources :revenus
   resources :fraispros
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
