70ed54933c8ae068b820a6bb932b413bb537ffe2
Olivier Lutz <olivierlutz8@gmail.com>
2020-10-26 18:09:45 +0000

    Ajout calcul page revenu + Graph

--- app/controllers/revenus_controller.rb
+++ app/controllers/revenus_controller.rb
@@ -1,34 +1,42 @@
 class RevenusController < ApplicationController
   include ActionView::Helpers::NumberHelper
   def index
-     @caca = Revenu.all
+     @revenu = Revenu.all
      @cacaNew = Revenu.new
+  end
+  
+  def update
+    @revenu = Revenu.find(params[:id])
+    @revenu.update(revenu_params)
+    redirect_to revenus_path, notice: "modification enregistrée"
+  end
+  
+  def show
   end
   
   def create
     @cacaNew = Revenu.new(revenu_params)
     @cacaNew.save
-    salaire = number_to_currency(Revenu.last.salaire, :unit => "€", locale: :fr, precision: 0)
-    revenuindep = number_to_currency(Revenu.last.revenuindep, :unit => "€", locale: :fr, precision: 0)
+    if @cacaNew.save
+        salaire = number_to_currency(Revenu.last.salaire, :unit => "€", locale: :fr, precision: 0)
+        revenuindep = number_to_currency(Revenu.last.revenuindep, :unit => "€", locale: :fr, precision: 0)
       redirect_to revenus_path, notice: "Revenu indépendant de " + revenuindep + " et salarié de " + salaire + " a bien été enregistré en base de donnée"
+    else
+      redirect_to revenus_path,  notice: @cacaNew.errors.full_messages
+    end
   end
   
   def destroy
     @revenu = Revenu.find(params[:id])
-    p = params[:revenu].inspect
     @revenu.destroy
+    redirect_to revenus_path, notice: 'Donnée supprimée' 
   end
     
-  def show
-    @revenu = Revenu.find(params[:id])
-    p = params[:revenu].inspect
-    redirect_to revenu_path, notice: (p + " ==> a été supprimé de la base de donnée")
-  end
   
   private
   
   def revenu_params
-    params.require(:revenu).permit(:salaire, :revenuindep)
+    params.require(:revenu).permit(:salaire, :revenuindep,:mois)
   end
   
 end
--- app/models/revenu.rb
+++ app/models/revenu.rb
@@ -1,2 +1,4 @@
 class Revenu < ApplicationRecord
+    validates :revenuindep, presence: { message: '==> vide' }
+    validates :salaire, presence: { message: '==> vide' }
 end
