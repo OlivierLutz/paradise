fc806f6ef39ab5a8576d4745d3c8ca937882e39b
Olivier Lutz <olivierlutz8@gmail.com>
2020-10-26 21:38:33 +0000

    Ajout authentification utilisateur

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'fraispros#index'
+  get 'home/index'
+  root to: "home#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  #root to: 'fraispros#index'
   get 'duplicate/:id', to: 'fraispros#duplicate', as: 'duplicate'
   resources :revenus
   resources :fraispros
